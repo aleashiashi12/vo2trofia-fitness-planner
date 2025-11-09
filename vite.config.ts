e7fc9f49b0a0cfb62cce69f303b1d73b464405bf
aleashiashi12 <aleashiashi12@gmail.com>
2025-11-09 16:59:23 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vo2trofia-fitness-planner/' // <-- ¡AÑADE ESTA LÍNEA!
+  base: '/vo2trofia-fitness-planner/' // <-- ¡Esta es la línea importante que añadimos!
 })
-
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
